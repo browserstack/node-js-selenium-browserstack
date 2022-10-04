71c06765334a982dedd5016b2fa41784dc950c32
Karan Shah <64479353+karanshah-browserstack@users.noreply.github.com>
2022-10-04 17:24:25 +0530

    Update tests/local.js

--- tests/local.js
+++ tests/local.js
@@ -5,7 +5,7 @@ const browserstack = require('browserstack-local');
 
 // Input capabilities
 const { localTestCapabilities, hubURL } = require('../conf');
-localTestCapabilities['bstack:options'].source = "node-js:sample-master:v1.0";
+localTestCapabilities['bstack:options'].source = "node-js:sample-main:v1.0";
 
 //creates an instance of Local
 const bs_local = new browserstack.Local();
